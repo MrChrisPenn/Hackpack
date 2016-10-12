8bbdf81ca1456efc5f07098f08c42a2803f02e93
ncscomputing <cnpenn@gmail.com>
2016-10-12 21:12:02 +0000

    Add files via upload

---
+++ sense emu wall.py
@@ -0,0 +1,60 @@
+
+"""
+Written by @ncscomputing always inspired by:
+David Whale
+Martin O'Hanlon and Craig Richardson
+"""
+from sense_emu import SenseHat
+
+#sense = SenseHat()
+
+
+    
+
+
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import random
+import time
+#from sense_hat import SenseHat
+
+
+sense = SenseHat()
+mc = minecraft.Minecraft.create()
+
+
+Block = 35
+WoolIdList=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+WallSize = 0
+
+
+
+def PrintWall():
+    WallSize = int(sense.temp)
+    msg = "Current Temp is: "+str(WallSize)
+    mc.postToChat(msg)
+    myList = [[0 for j in range(WallSize)]for i in range(WallSize)]
+    #pos = mc.player.getTilePos()
+    #mc.player.setPos(pos.x,pos.y,pos.z)
+
+    for row in range (0,WallSize):
+        for column in range (0,WallSize):
+            mc.setBlock(pos.x+row,pos.y+column,pos.z-20,Block,random.choice(WoolIdList))
+    return WallSize
+def ClearWall(WallSize,blockID):
+    myList = [[0 for j in range(WallSize)]for i in range(WallSize)]
+    #pos = mc.player.getTilePos()
+    #mc.player.setPos(pos.x,pos.y,pos.z)
+
+    for row in range (0,WallSize):
+        for column in range (0,WallSize):
+            mc.setBlock(pos.x+row,pos.y+column,pos.z-20,blockID)
+
+pos = mc.player.getTilePos()
+while True:
+    PrintWall()
+    time.sleep(5)
+    ClearWall(PrintWall(),0)
+    time.sleep(5)
+    
+        
